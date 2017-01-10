bb11d4350b760e37bac272999aa8632e12e534fe
iameld3st <iameld3st@gmail.com>
2017-01-10 10:56:00 -0500

    Actually did some stuff

---
+++ client/client.js
@@ -0,0 +1,102 @@
+var highQuality = true;
+var syncStatus = 0;
+var currentEpisode = 0;
+var serverTime = 0;
+var offset = 0;
+var episodesLeft = 0;
+var serverStatus = 0;
+var nick = 'Nickname';
+var player = videojs(document.getElementById('mainVideo'), { "controls": false, "autoplay": false, "preload": "auto" });
+var socket = io();
+
+function setupPlayer(){
+  player.src({ type: "video/mp4", src: "" });
+  player.on('ready', function(){player.currentTime(serverTime)});
+}
+setupPlayer();
+player.on('ready', function(){player.play()});
+
+var ui = setInterval(updateInfo, 100);
+function updateInfo(){
+  serverTime = player.currentTime();
+  // SyncStatus
+  if (syncStatus === 1) {
+    document.getElementById('syncStatus').innerHTML = 'Synced';
+  }else if (syncStatus === 2) {
+    document.getElementById('syncStatus').innerHTML = 'Unsynced';
+  }else{
+    document.getElementById('syncStatus').innerHTML = 'Unknown';
+  }
+  // CurrentQuality
+  if (highQuality) {
+    document.getElementById('currentQuality').innerHTML = 'High';
+  }else{
+    document.getElementById('currentQuality').innerHTML = 'Low';
+  }
+  // CurrentEpisode
+  document.getElementById('currentEpisode').innerHTML = currentEpisode;
+  // ServerTime
+  document.getElementById('serverTime').innerHTML = serverTime;
+  // YourTime
+  document.getElementById('yourTime').innerHTML = player.currentTime();
+  // TimeOffset
+  document.getElementById('timeOffset').innerHTML = serverTime - player.currentTime();
+  // ServerStatus
+  if (serverStatus === 0){
+    document.getElementById('serverStatus').innerHTML = 'Ready';
+  }else if (serverStatus === 1) {
+    document.getElementById('serverStatus').innerHTML = 'Playing';
+  }else if (serverStatus === 2) {
+    document.getElementById('serverStatus').innerHTML = 'Paused';
+  }else{
+    document.getElementById('serverStatus').innerHTML = 'Unknown';
+  }
+  // EpisodesLeft
+  document.getElementById('episodesLeft').innerHTML = episodesLeft;
+  // ReadyState
+  if (player.readyState() === 0) {
+    document.getElementById('readyState').innerHTML = 'Nothing';
+  }else if (player.readyState() === 1) {
+    document.getElementById('readyState').innerHTML = 'Metadata';
+  }else if (player.readyState() === 2) {
+    document.getElementById('readyState').innerHTML = 'Current Data';
+  }else if (player.readyState() === 3) {
+    document.getElementById('readyState').innerHTML = 'Future Data';
+  }else if (player.readyState() === 4) {
+    document.getElementById('readyState').innerHTML = 'Enough Data';
+  }else{
+    document.getElementById('readyState').innerHTML = 'Unknown';
+  }
+}
+
+socket.on('chatMessage', function (msg) {
+  document.getElementById('messages').innerHTML += msg;
+  $("#messages").scrollTop($("#messages")[0].scrollHeight);
+});
+
+$('#sendMessage').on('click', function () {
+  socket.emit('chatMessage', "<b>"+nick+"</b>: "+$('#messageContents').val());  
+  $('#messageContents').val('');
+})
+
+$('#changeNickname').on('click', function () {
+  socket.emit('chatMessage', "Nickname<b>Change</b> from "+nick+" to "+$('#nickname').val());
+  nick = $('#nickname').val();
+})
+
+$('#syncPlayer').on('click', function () {
+  player.currentTime(serverTime);
+})
+
+$('#reloadPlayer').on('click', function () {
+  //TODO: reload player
+})
+
+$('#qualitySel').on('click', function () {
+  highQuality = !highQuality;
+  if(highQuality){
+    document.getElementById('qualitySel').innerHTML = "Low<b>Quality</b>";
+  }else{
+    document.getElementById('qualitySel').innerHTML = "High<b>Quality</b>";
+  }
+})
--- server.js
+++ server.js
@@ -1,11 +1,41 @@
-var express = require('express')
-var app = express()
-var io = require('socket.io')();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var messageColor = false;
 
-app.listen(80, function(){console.log('Listening on port 80')});
-
-app.use(express.static('client'));
+http.listen(2712, function(){console.log('Listening on port 2712')});
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + 'client/index.html');
+  res.sendFile(__dirname + '/client/index.html');
+});
+
+app.get('/:path', function (req, res) {
+  res.sendFile(__dirname + '/client/' + req.params.path);
+});
+
+app.get('/source/:quality', function (req, res) {
+  res.send('{error:"WIP",quality:"'+ req.params.quality +'"}');
+});
+
+app.get('/current', function (req, res) {
+  res.send('{error:"WIP"}');
+});
+
+app.get('/info', function (req, res) {
+  res.send('{error:"WIP"}');
+});
+
+io.on('connection', function(socket){
+  console.log('User connected');
+  socket.on('disconnect', function(){
+    console.log('User disconnected');
+  });
+  socket.on('chatMessage', function(msg){
+    if(messageColor){
+      io.emit('chatMessage', '<span class="m1">'+ msg +"</span>");
+    }else{
+      io.emit('chatMessage', '<span class="m2">'+ msg +"</span>");
+    }
+    messageColor = !messageColor;
+  });
 });
